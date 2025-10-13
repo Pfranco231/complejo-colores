73188ad83a957aaa473085033325f12d49089cd8
Franco Papeschi <papeschifranco@gmail.com>
2025-10-13 18:56:38 -0300

    'kay men I copy all code from the original project to the new proyect(with react)

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,189 @@
-import { NavLink } from "react-router-dom";
 
 
 const Main = () => {
   return(
     <>
     <main>
+
+
+        <div  style={{  marginTop: "15px", textAlign: "center"}}>
+            <h1><strong>Colores de las Sierras</strong></h1>
+            <div  style={{ backgroundColor: "#DEB887", color: "white", padding: "25px", width: "60%", marginLeft: "20%", borderRadius: "25px" }}>
+                <h5><strong>En el corazón de las sierras de Córdoba, en un lugar único, se encuentra Colores de las Sierras. Un lugar para disfrutar de la naturaleza, la tranquilidad y la paz que solo las sierras pueden brindar. Colores de las Sierras es un complejo de cabañas ubicado en la localidad de La Paz, Córdoba. Un lugar ideal para descansar y disfrutar de la naturaleza en familia o con amigos. El complejo cuenta con departamentos, totalmente equipados con Wi-Fi, Aire Acondicionado, Cable, Etc. Con todas las comodidades para que su estadía sea inolvidable. Además, cuenta con una piscina al aire libre y un parque. En Colores de las Sierras, la naturaleza es la protagonista. Las Sierras, los sonidos de la naturaleza y la tranquilidad del lugar hacen de este complejo un lugar único en Córdoba, Argentina. Si está buscando un lugar para descansar y disfrutar de la naturaleza, Colores de las Sierras es el lugar indicado. ¡Lo esperamos!</strong></h5>
+            </div>
+        </div>
+
+        
+      <div className="places-container">
+        <center><h3 className="places-title">
+          <strong>Lugares Turísticos</strong>
+        </h3></center>
+        <div className="places">
+          <div className="card"  style={{  width: "18rem" }}>
+            <img src="imgs/lugares/la-paz.jpg" className="card-img-top" alt="..." height="191px" />
+            <div className="card-body">
+              <h5 className="card-title">La Paz - (lugar de las cabañas)</h5>
+               
+              <a id="onplaces1" className="btn"><strong>Ver Lugares</strong></a>
+            </div>
+          </div>
+          <div className="card"  style={{  width: "18rem" }}>
+            <img src="imgs/lugares/loma-bola.jpg" className="card-img-top" alt="..." height="191px" />
+            <div className="card-body">
+              <h5 className="card-title">Loma Bola - 3km</h5>
+               
+              <a id="onplaces2" className="btn"><strong>Ver Lugares</strong></a>
+            </div>
+          </div>
+          <div className="card"  style={{  width: "18rem" }}>
+            <img src="imgs/lugares/san-javier.jpg" className="card-img-top" alt="..." height="191px" />
+            <div className="card-body">
+              <h5 className="card-title">San Javier - 25km</h5>
+               
+              <a id="onplaces4" className="btn"><strong>Ver Lugares</strong></a>
+            </div>
+          </div>
+          <div className="card"  style={{  width: "18rem" }}>
+            <img src="imgs/lugares/villa-rosas.jpg" className="card-img-top" alt="..." height="191px" />
+            <div className="card-body">
+              <h5 className="card-title">Villa de Las Rosas - 39km</h5>
+               
+              <a id="onplaces5" className="btn"><strong>Ver Lugares</strong></a>
+            </div>
+          </div>
+          <div className="card"  style={{  width: "18rem" }}>
+            <img src="imgs/lugares/quebrada-pozos.jpg" className="card-img-top" alt="..." height="191px" />
+            <div className="card-body">
+              <h5 className="card-title">Quebrada de los Pozos - 55km</h5>
+               
+              <a id="onplaces6" className="btn"><strong>Ver Lugares</strong></a>
+            </div>
+          </div>
+          <div className="card"  style={{  width: "18rem" }}>
+            <img src="imgs/lugares/nono.jpg" className="card-img-top" alt="..." height="191px" />
+            <div className="card-body">
+              <h5 className="card-title">Nono - 62km</h5>
+               
+              <a id="onplaces7" className="btn"><strong>Ver Lugares</strong></a>
+            </div>
+          </div>
+          <div className="card"  style={{  width: "18rem" }}>
+            <img src="imgs/lugares/las-rabonas.jpg" className="card-img-top" alt="..." height="191px" />
+            <div className="card-body">
+              <h5 className="card-title">Las Rabonas - 65km</h5>
+               
+              <a id="onplaces8" className="btn"><strong>Ver Lugares</strong></a>
+            </div>
+          </div>
+          <div className="card"  style={{  width: "18rem" }}>
+            <img src="imgs/lugares/mina-clavero.jpg" className="card-img-top" alt="..." height="191px" />
+            <div className="card-body">
+              <h5 className="card-title">Mina Clavero - 82km</h5>
+               
+              <a id="onplaces9" className="btn"><strong>Ver Lugares</strong></a>
+            </div>
+          </div>
+          <div className="card"  style={{  width: "18rem" }}>
+            <img src="imgs/lugares/cura-brochero.jpg" className="card-img-top" alt="..." height="191px" />
+            <div className="card-body">
+              <h5 className="card-title">Villa Cura Brochero - 84km</h5>
+               
+              <a id="onplaces10" className="btn"><strong>Ver Lugares</strong></a>
+            </div>
+          </div>
+          
+        </div>
+        </div>
+
+        <center><h3 style={{  marginTop: "25px", marginBottom: "25px"  }}><strong>Lugares para comer</strong></h3></center>
+        <div className="places">
+          <div className="card"  style={{  width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">El Ciervo</h5>
+               
+              <a className="btn"><strong>Ver Restaurant</strong></a>
+            </div>
+          </div>
+
+          <div className="card"  style={{  width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">La Catalina</h5>
+               
+              <a className="btn"><strong>Ver Restaurant</strong></a>
+            </div>
+          </div>
+
+          <div className="card"  style={{  width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">El fogon</h5>
+               
+              <a className="btn"><strong>Ver Restaurant</strong></a>
+            </div>
+          </div>
+
+          <div className="card"  style={{  width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">No te metas con mi hermana</h5>
+               
+              <a className="btn"><strong>Ver Restaurant</strong></a>
+            </div>
+          </div>
+
+          <div className="card"  style={{  width: "18rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">La Galera</h5>
+               
+              <a className="btn"><strong>Ver Restaurant</strong></a>
+            </div>
+          </div>
+      </div>
+        
+
+                <center id="img-place">
+            <h3><strong>Imágenes</strong></h3>
+          </center>
+      <div id="carousel" className="carousel">
+
+        <div id='item_1' className="hideLeft">
+          <img src="imgs/complejo/1.jpg" height="340" />
+        </div>
+
+        <div id='item_2' className="prevLeftSecond">
+          <img src="imgs/complejo/2.jpg" height="340" />
+        </div>
+
+        <div id='item_3' className="prev">
+          <img src="imgs/complejo/3.jpg" height="340" />
+        </div>
+
+        <div id='item_4' className="selected">
+          <img src="imgs/complejo/4.jpg" height="340" />
+        </div>
+
+        <div id='item_5' className="next">
+          <img src="imgs/complejo/5.jpg" height="340" />
+        </div>
+
+        <div id='item_6' className="nextRightSecond">
+          <img src="imgs/complejo/6.jpg" height="340" />
+        </div>
+
+        <div id='item_7' className="hideRight">
+          <img src="imgs/complejo/img.jpg" height="340" />
+        </div>
+
+
+      </div>
+
+      <div className="buttons">
+    	  <button className="button-82-pushable" role="button" id="prev"><span className="button-82-shadow"></span><span className="button-82-edge"></span><span className="button-82-front text">Anterior</span></button>
+
+
+    	  <button className="button-82-pushable" role="button" id="next"><span className="button-82-shadow"></span><span className="button-82-edge"></span><span className="button-82-front text">Próximo</span></button>
+      </div>
+
+
+        
     </main>
     </>
   )
